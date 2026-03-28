4f747f7a05a6c82996e7e94879bc3bd88a014539
shridattadixitcs25-ui <shridattadixit.cs25@bmsce.ac.in>
2026-03-28 16:40:04 +0530

    time of commit added in title

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+import time  
 cmd = input()
 cmd = cmd.split()
 if cmd[0]=="com":
@@ -19,9 +20,9 @@ if cmd[0]=="com":
                     break
                 sha1.update(data)
         hashed = sha1.hexdigest()
-        fle_path = "commits\\"+str(hashed)
+        fle_path = "commits\\"+str(hashed)+""+str(time.localtime().tm_mday())+str(time.localtime().tm_mon())+str(time.localtime().tm_year())+str(time.localtime().tm_hour())+str(time.localtime().tm_min())+str(time.localtime().tm_sec())
         newfile = open(fle_path,'w')
         fle = open(fle,'r')
         for line in fle:
             newfile.write(line)
-            print(line)
+        newfile.close()
